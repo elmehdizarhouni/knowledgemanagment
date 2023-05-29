550b7ed80ddcb5c66281b16eb4ce11df9864e396
elmehdi <elmehdizarhouni@gmail.com>
2023-05-29 22:44:11 +0100

    fifth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,11 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'name'=>'User1',
+            'email'=>'user1@email.com',
+            'type'=>'Evaluateur',
+            'password'=>bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'name'=>'User2',
+            'email'=>'user2@email.com',
+            'type'=>'Employé',
+            'password'=>bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'name'=>'User3',
+            'email'=>'user3@email.com',
+            'type'=>'Employé',
+            'password'=>bcrypt('password'),
+        ]);
     }
 }
